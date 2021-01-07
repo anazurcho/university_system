4cd58e54197aa5acee66deaa1070f11257edfedb
anazurcho <ana.zurabashvili.1@btu.edu.ge>
2021-01-07 05:32:41 +0400

    add course requests change migrations

--- database/migrations/2020_12_26_235208_create_student_shells_table.php
+++ database/migrations/2020_12_26_235208_create_student_shells_table.php
@@ -6,28 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateStudentShellsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
+
         Schema::create('student_shells', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('user_id');
+            $table->float('total_score');
             $table->unsignedInteger('lecture_id');
             $table->unique(array('user_id', 'lecture_id'));
-            $table->float('total_score');
             $table->timestamps();
         });
     }
 //$table->unique(array('user_id', 'lecture_id'));
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+
     public function down()
     {
         Schema::dropIfExists('student_shells');
